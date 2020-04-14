03fb63d971706847bd6d3de00228432b5057e82e
Gabriel De Moura <gabrielmoura.music@gmail.com>
2020-04-14 00:51:42 -0300

    inicial commit

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,20 @@
+import { uuid } from 'uuidv4';
+import { Router } from 'express';
+
+const appointmentsRouter = Router();
+
+const appointments = [];
+appointmentsRouter.post('/', (req, res) => {
+  const { provider, date } = req.body;
+
+  const appointment = {
+    id: uuid(),
+    provider,
+    date,
+  };
+
+  appointments.push(appointment);
+  return res.json(appointment);
+});
+
+export default appointmentsRouter;
