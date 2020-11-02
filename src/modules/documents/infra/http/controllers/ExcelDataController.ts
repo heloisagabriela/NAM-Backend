6883edc8c236ad12b71b71209571b3208c0cfec4
Gabriel de Moura <gabrielmoura.music@gmail.com>
2020-11-02 11:51:55 -0300

    Added Feature

---
+++ src/modules/documents/infra/http/controllers/ExcelDataController.ts
@@ -0,0 +1,80 @@
+/* eslint-disable consistent-return */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable func-names */
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+/* eslint-disable no-useless-concat */
+import CreateCollectionService from '@modules/collection/service/CreateCollectionService';
+import GetDocumentDataService from '@modules/documents/services/GetDocumentDataService';
+import GetDocumentSchemaService from '@modules/documents/services/GetDocumentSchemaService';
+import excel from 'exceljs';
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+interface IDocProps {
+  titulo: string;
+  autor: string;
+  pasta_documento: string;
+  observacoes: string;
+  pasta: string;
+}
+
+interface IDocSchemaProps {
+  column_name: string;
+  data_type: string;
+}
+
+export default class ExcelDataController {
+  async show(request: Request, response: Response) {
+    const { collectionId } = request.params;
+
+    const getDocumentDataService = new GetDocumentDataService(collectionId);
+
+    const documentsData = (await getDocumentDataService.execute()) as IDocProps[];
+
+    const getDocumentSchemaService = new GetDocumentSchemaService(collectionId);
+
+    const documentScheema = (await getDocumentSchemaService.execute()) as IDocSchemaProps[];
+
+    const columns = documentScheema
+      .map(schema => {
+        let header =
+          schema.column_name.charAt(0).toUpperCase() +
+          schema.column_name.slice(1);
+        header = header.replace('_', ' ');
+
+        return {
+          header,
+          key: schema.column_name,
+          width: 25,
+        };
+      })
+      .filter(schema => schema.key !== 'id');
+
+    const workbook = new excel.Workbook() as any;
+    const worksheet = workbook.addWorksheet('Documentos');
+
+    worksheet.columns = columns;
+
+    worksheet.addRows(documentsData);
+
+    const collectionService = container.resolve(CreateCollectionService);
+
+    const collection = await collectionService.getById({ id: collectionId });
+
+    const collectionName = collection?.name.replace(' ', '_').toLowerCase();
+
+    response.setHeader(
+      'Content-Type',
+      'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+    );
+
+    response.setHeader(
+      'Content-Disposition',
+      `attachment; filename=documentos_acervo_${collectionName}.xlsx`,
+    );
+
+    return workbook.xlsx.write(response).then(function () {
+      response.status(200).end();
+    });
+  }
+}
--- src/modules/documents/infra/http/routes/documents.routes.ts
+++ src/modules/documents/infra/http/routes/documents.routes.ts
@@ -1,19 +1,46 @@
 import { Router } from 'express';
+import ensureAuthenticated from '@modules/users/infra/midddlewares/ensureAuthenticated';
 import DocumentsDataController from '../controllers/DocumentDataController';
 // import { celebrate, Segments, Joi } from 'celebrate';
 import DocumentsSchemaController from '../controllers/DocumentsSchemasController';
+import ExcelDataController from '../controllers/ExcelDataController';
+import ExcelSchemaController from '../controllers/ExcelSchemaController';
 
 const documentsSchemaController = new DocumentsSchemaController();
 const documentsDataController = new DocumentsDataController();
-
+const excelDataController = new ExcelDataController();
+const excelSchemaController = new ExcelSchemaController();
 const documentsRouter = Router();
 
-documentsRouter.get('/structure/:collectionId', documentsSchemaController.show);
+documentsRouter.get(
+  '/structure/:collectionId',
+  ensureAuthenticated,
+  documentsSchemaController.show,
+);
 
-documentsRouter.post('/structure/', documentsSchemaController.create);
+documentsRouter.post(
+  '/structure/',
+  ensureAuthenticated,
+  documentsSchemaController.create,
+);
 
-documentsRouter.post('/data/:collectionId', documentsDataController.create);
+documentsRouter.get(
+  '/structure/excel/:collectionId',
+  excelSchemaController.show,
+);
 
-documentsRouter.get('/data/:collectionId', documentsDataController.index);
+documentsRouter.post(
+  '/data/:collectionId',
+  ensureAuthenticated,
+  documentsDataController.create,
+);
+
+documentsRouter.get(
+  '/data/:collectionId',
+  ensureAuthenticated,
+  documentsDataController.index,
+);
+
+documentsRouter.get('/data/excel/:collectionId', excelDataController.show);
 
 export default documentsRouter;
